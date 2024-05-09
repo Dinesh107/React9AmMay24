f9d1fa8daf53f9d82becc2461fba0bd3b87020bb
Dinesh107 <dineshjr107@gmail.com>
2024-05-09 10:31:03 +0530

    FirstCommit

---
+++ src/components/WelcomeClassCompPractice.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class WelcomeStudents extends React.Component {
+
+    render() {
+
+        return <h1>Welcome Students To the React Class, Class Comp</h1>
+
+    }
+
+}
+
+export default WelcomeStudents;
---
+++ src/components/WelcomeFunctionalCompPractice.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Welcome() {
+
+   return <h1>Welcome Students To the React Class, Functional Comp</h1>
+
+}
+
+export default Welcome;
+
+
+

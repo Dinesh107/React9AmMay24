721447e17d14d638eb559551b3b99fc701298430
Dinesh107 <dineshjr107@gmail.com>
2024-05-10 10:31:19 +0530

    SecondCommit

--- src/components/WelcomeFunctionalCompPractice.js
+++ src/components/WelcomeFunctionalCompPractice.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function Welcome() {
-
-   return <h1>Welcome Students To the React Class, Functional Comp</h1>
-
+const Welcome = (props) => {
+console.log(props)
+    return (
+        <div>
+            <h1>Hello I am {props.name}, I play {props.sportsName}</h1>
+            {props.children}
+        </div>
+    )
 }
-
 export default Welcome;
 
 
